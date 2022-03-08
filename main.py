c091468151f7acdc26343016c6d4c66511a5970d
Neil Mehra <neilmehra@outlook.com>
2022-03-08 15:56:05 -0500

    :sob:

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from mailbox import Maildir
 import fire
 import json
 import requests
 
 class Manga:
 
-    def __init__(self, title='', mal_link='', demographic='', genres=[], score = -1) -> None:
+    def __init__(self, title='', mal_link='', demographic='', genres=[], score = -1, last_chapter = -1) -> None:
         self.title = title
         self.mal_link = mal_link
         self.demographic = demographic
         self.genres = genres
         self.score = score
+        self.last_chapter = last_chapter
 
     def add_to_json(json):
         print()
@@ -23,16 +25,22 @@ class Manga:
 def update_manga_txt():
     print()
 
+def get_client_id():
+    ret = ''
+    with open('./id.txt', 'r') as file:
+         ret = file.read().strip()
+    return ret
+
 def clear_manga():
-    with open('./tracked_manga.txt', 'r') as file:
-        with open('./tracked_manga_backup.txt', 'w') as backup_file:
+    with open('./data/tracked_manga.txt', 'r') as file:
+        with open('./data/tracked_manga_backup.txt', 'w') as backup_file:
             backup_file.writelines(file.readlines())
-    file = open('./tracked_manga.txt', 'w')
+    file = open('./data/tracked_manga.txt', 'w')
     file.close()
 
 def get_tracked_manga():
     ret = {}
-    with open('./tracked_manga.txt', 'r') as file:
+    with open('./data/tracked_manga.txt', 'r') as file:
         line = file.readline()
         try:
             json.loads(line)
@@ -42,7 +50,7 @@ def get_tracked_manga():
     return ret
 
 def write_tracked_manga(to_write):
-    with open('./tracked_manga.txt', 'w') as file:
+    with open('./data/tracked_manga.txt', 'w') as file:
         file.write(json.dumps(to_write))
 
 def track(manga_name: str):
@@ -51,9 +59,15 @@ def track(manga_name: str):
     :param manga_name: the name of the manga
     :return: none
     """
-    base = 'https://kitsu.io/api/edge/anime?filter[text]='
-    manga_formatted = manga_name.replace(' ', r'%20')
-    print(base + manga_formatted)
+    MAL_HEADER = 'X-MAL-CLIENT-ID'
+    header = {MAL_HEADER:get_client_id()}
+    limit = 1
+    base = 'https://api.myanimelist.net/v2/anime?q='
+    query = manga_name.replace(' ', r'%20')
+    api = base + query + 'limit=' + str(limit)
+    r = requests.get(api, header)
+    print(r.content)
+    print(get_client_id())
 
 tracked_manga = get_tracked_manga()
 
